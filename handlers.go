eff828a48e3d2d0c31769634d6380610cd3f3271
Jacob <25340202+Jacob2017@users.noreply.github.com>
2026-01-04 08:22:59 +0000

    up to reset

--- handlers.go
+++ handlers.go
@@ -1,1 +1,95 @@
 package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type commands struct {
+	Handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if handler, ok := c.Handlers[cmd.name]; ok {
+		err := handler(s, cmd)
+		if err != nil {
+			return fmt.Errorf("Error: %v", err)
+		}
+		return nil
+	}
+	return fmt.Errorf("Command %s not found", cmd.name)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.Handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("No arguments provided")
+	}
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("SQL error - %v", err)
+	}
+
+	s.cfg.SetUser(user.Name)
+	fmt.Printf("User has been set to %s\n", cmd.args[0])
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("No arguments provided")
+	}
+
+	newUser := struct {
+		ID        uuid.NullUUID
+		CreatedAt sql.NullTime
+		UpdatedAt sql.NullTime
+		Name      string
+	}{
+		ID: uuid.NullUUID{
+			UUID:  uuid.New(),
+			Valid: true,
+		},
+		CreatedAt: sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		},
+		UpdatedAt: sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		},
+		Name: cmd.args[0],
+	}
+
+	user, err := s.db.CreateUser(context.Background(), newUser)
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+	s.cfg.CurrentUser = cmd.args[0]
+	fmt.Println("User created")
+	fmt.Println(user)
+
+	s.cfg.SetUser(user.Name)
+	fmt.Printf("User has been set to %s\n", cmd.args[0])
+
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.ResetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+	fmt.Println("`users` table reset.")
+	return nil
+
+}
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/jacob2017/gator/internal/config"
+	"github.com/jacob2017/gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,7 +23,40 @@ type command struct {
 
 func main() {
 	cfg := config.Read()
-	// cfg.SetUser("Jacob")
-	fmt.Println(cfg)
+	db, err := sql.Open("postgres", cfg.DBURL)
+	dbQueries := database.New(db)
+	st := state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
+	cmds := commands{
+		Handlers: make(map[string]func(*state, command) error),
+	}
+	// Register
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
+	fmt.Println(os.Args)
+	fmt.Println(len(os.Args))
+
+	if len(os.Args) < 2 {
+		fmt.Println("Error: Not enough arguments")
+		os.Exit(1)
+	}
+	inputArgs := make([]string, len(os.Args))
+	copy(inputArgs, os.Args)
+	cmd := command{
+		name: inputArgs[1],
+		args: inputArgs[2:],
+	}
+	// if len(os.Args) > 2 {
+	// 	copy(cmd.args, os.Args[1:])
+	// }
+	fmt.Println(cmd)
+
+	err = cmds.run(&st, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
